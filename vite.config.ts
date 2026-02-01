ff8f498fdeca3c58857c2a5f0f31c6863b22981d
georgerob801 <86919579+georgerob801@users.noreply.github.com>
2026-02-01 01:28:50 +0000

    fix: fuieakdeg,yKU,\FGJGEW

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
     plugins: [reactRouter(), tsconfigPaths()],
     server: {
-        host: true
+        host: true,
+        allowedHosts: [
+            "ichack.unigeorge.uk"
+        ]
     }
 });
